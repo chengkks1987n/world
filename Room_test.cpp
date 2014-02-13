40c5f4b918ae23b42794a258e30836bbbacd65e5
cheky <chengkechengke@gmail.com>
2014-02-13 08:57:36 -0500

    finish the test for Room.

--- Room_test.cpp
+++ Room_test.cpp
@@ -22,10 +22,25 @@ BOOST_AUTO_TEST_CASE( test_Room )
   world::Thing t("wand", &r);
   BOOST_CHECK(t.getName() == "wand");
   BOOST_CHECK(t.getLocation() == &r);
+  
+  BOOST_CHECK(*(r.getThings().begin()) == &t);
   BOOST_CHECK(r.getThings().size() == 1);
-  
+  BOOST_CHECK(*(r.thingNamed("wand").begin()) == &t);
   BOOST_CHECK(r.thingNamed("wand").size() == 1);
-
   
+  world::Thing t1("book", &r);
+  BOOST_CHECK(r.getThings().size() == 2);
+  BOOST_CHECK(*(r.thingNamed("wand").begin()) == &t);
+  BOOST_CHECK(r.thingNamed("wand").size() == 1);
+  BOOST_CHECK(*(r.thingNamed("book").begin()) == &t1);
+  BOOST_CHECK(r.thingNamed("book").size() == 1);
+
+  BOOST_CHECK(r.thingNamed("empty").size() == 0);
+  
+  world::Thing t2("book", &r);
+  BOOST_CHECK(r.getThings().size() == 3);
+  BOOST_CHECK(*(r.thingNamed("wand").begin()) == &t);
+  BOOST_CHECK(r.thingNamed("wand").size() == 1);
+  BOOST_CHECK(r.thingNamed("book").size() == 2);
 
 }
